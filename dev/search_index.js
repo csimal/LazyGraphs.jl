a706338314e158516ac700e3075c4f3ee0720a9b
Documenter.jl <documenter@juliadocs.github.io>
2022-02-28 22:36:17 +0000

    build based on d7b70b4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LazyGraphs","category":"page"},{"location":"#LazyGraphs","page":"Home","title":"LazyGraphs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LazyGraphs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LazyGraphs]","category":"page"}]
+}
